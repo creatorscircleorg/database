649baa5dda88ed0ccb6eb0e98c1960972a15a36e
lilymzhou <lilyzhou@stanford.edu>
2019-07-24 21:37:25 -0700

    Updates

---
+++ precache-manifest.42754b9a28831f1266dca4455755c191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aab929917eac7a5770af6985bf1e2ee",
+    "url": "/database/index.html"
+  },
+  {
+    "revision": "b8b6b5253210a2e5ed84",
+    "url": "/database/static/css/main.469cf3ef.chunk.css"
+  },
+  {
+    "revision": "3d4f185fd1a5ac3e1309",
+    "url": "/database/static/js/2.bc68d49c.chunk.js"
+  },
+  {
+    "revision": "b8b6b5253210a2e5ed84",
+    "url": "/database/static/js/main.354874a0.chunk.js"
+  },
+  {
+    "revision": "569a9be75cfd74460fc9",
+    "url": "/database/static/js/runtime~main.e63b407d.js"
+  }
+]);
