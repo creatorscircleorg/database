2f38296e2793bbaa8998c6afc9c69bfd33dc6666
creatorscircleorg <nayelli@creatorscircle.org>
2019-08-28 09:53:21 -0400

    Updates

--- precache-manifest.e984ff2efd50a94bfc6b60219a0160d1.js
+++ precache-manifest.e984ff2efd50a94bfc6b60219a0160d1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adfaaa15185ff2d19041779aa34fecbb",
+    "revision": "eeab1218d0d2cfbfcc1c7e87d329c453",
     "url": "/database/index.html"
   },
   {
-    "revision": "d9533cbffd89357b0e93",
+    "revision": "fdf1fa0063525d8bda10",
     "url": "/database/static/css/main.ed286826.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/database/static/js/2.de8a6dc3.chunk.js"
   },
   {
-    "revision": "d9533cbffd89357b0e93",
-    "url": "/database/static/js/main.fb1550a0.chunk.js"
+    "revision": "fdf1fa0063525d8bda10",
+    "url": "/database/static/js/main.2a7055a5.chunk.js"
   },
   {
     "revision": "569a9be75cfd74460fc9",
