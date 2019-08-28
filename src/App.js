fd9a2690fc9d7ca405fbd4b850228829fefafd0a
creatorscircleorg <53021372+creatorscircleorg@users.noreply.github.com>
2019-08-28 10:02:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ class App extends React.Component {
                     fontWeight: "bold",
                 },
                 root: {
-                    fontFamily: "Arial",
+                    fontFamily: "Verdana",
                 },
                 footer: {
                     position: "relative",
@@ -142,7 +142,7 @@ class App extends React.Component {
                 }
             },
             root: {
-                fontFamilySansSerif: "Arial",
+                fontFamilySansSerif: "Verdana",
             }
         }
     })
