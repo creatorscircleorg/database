c0a2dc708c49f99d0be6fe106fb0a77b9c711071
Lucia Zheng <zlucia@stanford.edu>
2020-12-18 15:19:11 -0800

    Updates

--- precache-manifest.a987f81f57c037b12751b6a1e7ace6f4.js
+++ precache-manifest.a987f81f57c037b12751b6a1e7ace6f4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38fc5902c415b64132d68b372727bc99",
+    "revision": "05c28fccf2734c3a07dccb1587c47861",
     "url": "/database/index.html"
   },
   {
-    "revision": "de99777bf3ddd0ba1872",
-    "url": "/database/static/css/main.df546ff8.chunk.css"
+    "revision": "c5aa77e573e8f6061cb2",
+    "url": "/database/static/css/main.506099b2.chunk.css"
   },
   {
     "revision": "50e3d5782fa3a343770c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/database/static/js/2.d5f85e7a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de99777bf3ddd0ba1872",
+    "revision": "c5aa77e573e8f6061cb2",
     "url": "/database/static/js/main.08719562.chunk.js"
   },
   {
